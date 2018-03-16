76c3905e0913c1415fc5122be95eae477afe92cc
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2018-03-16 12:39:27 +0100

    Initial configuration.

--- truffle.js
+++ truffle.js
@@ -1,4 +1,7 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  development: {
+    host: "127.0.0.1",
+    port: 8545,
+    network_id: "*" // match any network
+  },
 };
