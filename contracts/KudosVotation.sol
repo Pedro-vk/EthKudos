a932a5fd848bbde1b4915e9ad0d96e610669d025
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2018-03-16 18:57:57 +0100

    Initial KudosVotation contract.

---
+++ contracts/KudosVotation.sol
@@ -0,0 +1,70 @@
+pragma solidity ^0.4.18;
+
+import 'zeppelin/contracts/math/SafeMath.sol';
+import 'zeppelin/contracts/token/BurnableToken.sol';
+import 'zeppelin/contracts/ownership/Ownable.sol';
+
+contract KudosVotation is BurnableToken, Ownable {
+
+  using SafeMath for uint256;
+
+  string public version = '0.0.1';
+
+  string public name;
+  string public symbol;
+  uint8 public decimals;
+  uint256 public totalSupply;
+
+  uint256 public kudosByMember;
+  uint256 public maxKudosToMember;
+  address[] public members;
+
+  mapping (address => uint256) balances;
+  mapping (address => mapping (address => uint256)) allowed;
+
+  event AddMember(address indexed member);
+
+  function KudosVotation(
+    string _tokenName,
+    string _tokenSymbol,
+    uint8 _decimalUnits,
+    uint256 _kudosByMember,
+    uint256 _maxKudosToMember
+  ) public {
+    name = _tokenName;
+    symbol = _tokenSymbol;
+    decimals = _decimalUnits;
+    kudosByMember = _kudosByMember;
+    maxKudosToMember = _maxKudosToMember;
+  }
+
+  function addMember(address _member) onlyOwner public returns (bool) {
+    require(!isMember(_member));
+
+    members.push(_member);
+    balances[_member] = kudosByMember;
+    totalSupply += kudosByMember;
+    return true;
+  }
+
+  function isMember(address _member) public constant returns (bool) {
+    for (uint i = 0; i < members.length; i++) {
+      if (members[i] == _member) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function getMembers() public constant returns (address[]) {
+    return members;
+  }
+
+  function getMember(uint256 _index) public constant returns (address) {
+    return members[_index];
+  }
+
+  function membersNumber() public constant returns (uint256) {
+    return members.length;
+  }
+}
---
+++ test/KudosVotation.js
@@ -0,0 +1,61 @@
+var KudosVotation = artifacts.require('./KudosVotation.sol');
+
+contract('KudosVotation', accounts => {
+  let contract;
+
+  before(() => {
+    contract = KudosVotation.new('Kudos votation #1', 'KV1', 2, 5, 2);
+  });
+
+  it('should have 0 of total supply', async () => {
+    const instance = await contract;
+    const totalSupply = await instance.totalSupply();
+
+    assert.equal(totalSupply.toNumber(), 0, '0 wasn\'t in the total supply');
+  });
+
+  it('should be able to add members', async () => {
+    const instance = await contract;
+    const getMembersNumber = async () => (await instance.membersNumber()).toNumber();
+
+    assert.equal(await getMembersNumber(), 0, '0 wasn\'t the members number');
+
+    await instance.addMember(accounts[0]);
+    assert.equal(await getMembersNumber(), 1, '1 wasn\'t the members number');
+  });
+
+  it('should add the default amount of Kudos to the new member', async () => {
+    const instance = await contract;
+    const totalSupply = await instance.totalSupply();
+
+    assert.equal(totalSupply.toNumber(), 5, '5 wasn\'t in the total supply');
+  });
+
+  it('should prevent to add a member twice', async () => {
+    const instance = await contract;
+    const getMembersNumber = async () => (await instance.membersNumber()).toNumber();
+
+    let failed = false;
+    try {
+      await instance.addMember(accounts[0]);
+    } catch (e) {
+      failed = true;
+    }
+    assert.ok(failed, 'Must throw an error when the same memeber is added');
+
+    await instance.addMember(accounts[1]);
+    assert.equal(await getMembersNumber(), 2, '2 wasn\'t the members number');
+  });
+
+  it('should prevent the addition of members if is not the owner', async () => {
+    const instance = await contract;
+    let failed = false;
+
+    try {
+      await instance.addMember(accounts[3], {from: accounts[1]});
+    } catch (e) {
+      failed = true;
+    }
+    assert.ok(failed, 'Must throw an error when the a different address than owner adds a new member');
+  });
+});
