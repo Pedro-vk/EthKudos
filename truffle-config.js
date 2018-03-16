1e6037d42cbf501cbe6230866dd107396114925c
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2018-03-16 18:57:37 +0100

    Truffle configuration and EthPM dependencies added.

---
+++ truffle-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  networks: {
+    dev: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // match any network
+    },
+  },
+};
--- truffle.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  development: {
-    host: "127.0.0.1",
-    port: 8545,
-    network_id: "*" // match any network
-  },
-};
