7cae1923c21a048226171d25ebe4df3fd22b1844
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2018-03-29 00:28:34 +0200

    Fixed pending transaction flickering on MetaMask pending transactions workaround.

--- src/app/shared/web3.service.ts
+++ src/app/shared/web3.service.ts
@@ -14,6 +14,7 @@ import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/scan';
 import 'rxjs/add/operator/share';
 import 'rxjs/add/operator/shareReplay';
 import 'rxjs/add/operator/startWith';
@@ -87,6 +88,15 @@ export class Web3Service {
     .mergeMap(() => Observable.fromPromise(this.web3.eth.getBlock('pending', true)))
     .combineLatest(this.account$)
     .map(([{transactions}, account]) => transactions.filter(transaction => transaction.from.toLowerCase() === account.toLowerCase()))
+    .map(transactions => transactions.map(tx => tx.hash))
+    .distinctUntilChanged((a, b) => a.join('|') === b.join('|'))
+    .scan((acc, transactions) =>
+      [...acc, ...transactions]
+        .filter((tx, i, list) => list.indexOf(tx) === i)
+    , [])
+    .map(transactions => transactions.map(async tx => await this.web3.eth.getTransaction(tx)))
+    .mergeMap(_ => Observable.fromPromise(Promise.all(_)))
+    .map(transactions => transactions.filter(tx => !tx.blockNumber))
     .distinctUntilChanged((a, b) => a.map(_ => _.hash).join('|') === b.map(_ => _.hash).join('|'))
     .map(transactions =>
       transactions
