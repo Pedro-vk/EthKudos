d6996cb7e9cb0087ff9fc9af9c658a7a69e3585d
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2018-03-31 01:32:59 +0200

    Updated Zeppelin dependency.

--- contracts/KudosOrganisations.sol
+++ contracts/KudosOrganisations.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.18;
 
-import "zeppelin/contracts/ownership/Ownable.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./KudosRouter.sol";
 import "./KudosTokenFactory.sol";
 
--- contracts/KudosTokenFactory.sol
+++ contracts/KudosTokenFactory.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.18;
 
-import "zeppelin/contracts/token/BasicToken.sol";
-import "zeppelin/contracts/ownership/Ownable.sol";
+import "zeppelin-solidity/contracts/token/ERC20/BasicToken.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./string-utils.sol";
 import "./KudosRouter.sol";
 import "./Kudos.structs.sol";
