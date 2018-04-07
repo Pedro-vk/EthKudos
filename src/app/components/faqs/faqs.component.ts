4bab88caba359d2ed8e9b41bc1be8ef27f6c4bd6
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2018-04-08 00:00:21 +0200

    Inital FAQs just with some content, no style.

---
+++ src/app/components/faqs/faqs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eth-kudos-faqs',
+  templateUrl: './faqs.component.html',
+  styleUrls: ['./faqs.component.scss']
+})
+export class FaqsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,14 +1,17 @@
 export * from './blockie/blockie.component';
+export * from './faqs/faqs.component';
 export * from './help-cards/help-cards.component';
 export * from './share-dialog/share-dialog.component';
 
 import { BlockieComponent } from './blockie/blockie.component';
+import { FaqsComponent } from './faqs/faqs.component';
 import { HelpCardsComponent } from './help-cards/help-cards.component';
 
 import { ShareDialogComponent } from './share-dialog/share-dialog.component';
 
 export const COMPONENTS = [
   BlockieComponent,
+  FaqsComponent,
   HelpCardsComponent,
 ];
 export const ENTRY_COMPONENTS = [
