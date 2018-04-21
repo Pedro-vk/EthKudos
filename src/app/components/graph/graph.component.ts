8341129730fc720b9e144f38ab1e3ed37acf82ad
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2018-04-21 03:12:01 +0200

    Changed Graph layout on large mode.

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -88,7 +88,24 @@ export class GraphComponent implements OnInit {
           })),
       },
       style: this.style,
-      layout: {
+      layout: this.large ? {
+        name: 'cose',
+        idealEdgeLength: 100,
+        nodeOverlap: 20,
+        refresh: 20,
+        fit: true,
+        padding: 30,
+        randomize: false,
+        componentSpacing: 100,
+        nodeRepulsion: 1000000,
+        edgeElasticity: 100,
+        nestingFactor: 5,
+        gravity: 80,
+        numIter: 1000,
+        initialTemp: 200,
+        coolingFactor: 0.95,
+        minTemp: 1.0
+      } : {
         name: 'circle',
         directed: true,
         fit: this.large,
