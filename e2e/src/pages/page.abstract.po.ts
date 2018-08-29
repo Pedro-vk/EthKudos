21186184f8f8d4223994ba2e792e1c503a20196e
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2018-08-30 01:03:04 +0200

    Complete e2e testing refactor.

---
+++ e2e/src/pages/page.abstract.po.ts
@@ -0,0 +1,12 @@
+import { browser, by, element } from 'protractor';
+
+export abstract class Page {
+
+  abstract navigateTo(params?: string | number | {[param: string]: string | number}): Promise<any>;
+
+  protected async goToPath(path: string, wait: number = 0) {
+    await browser.waitForAngularEnabled(false);
+    await browser.get(path);
+    await browser.sleep(wait);
+  }
+}
