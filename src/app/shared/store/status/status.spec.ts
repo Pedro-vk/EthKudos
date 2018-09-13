87327448808346140070f5b0fee533b5a37695f9
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2018-09-14 00:01:33 +0200

    Added status effects. #97

--- src/app/shared/store/status/status.spec.ts
+++ src/app/shared/store/status/status.spec.ts
@@ -1,5 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { StoreModule, Store } from '@ngrx/store';
+import { ROOT_EFFECTS_INIT } from '@ngrx/effects';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Observable } from 'rxjs/Observable';
+import { hot, cold } from 'jasmine-marbles';
+
+import { PROVIDERS } from '../../';
 import { State, reducers } from '../';
 
 import { reduceActions } from '../testing-utils';
@@ -9,6 +15,7 @@ import { ConnectionStatus } from '../../web3.service';
 import { statusReducer } from './status.reducers';
 import * as fromStatus from './status.reducers';
 import * as statusActions from './status.actions';
+import { StatusEffects } from './status.effects';
 
 describe('Status - Reducers', () => {
   it('should be auto-initialized', () => {
@@ -33,5 +40,53 @@ describe('Status - Reducers', () => {
       ConnectionStatus.NoAccount,
       ConnectionStatus.Total,
     ]);
+  });
+});
+
+describe('Status - Effects', () => {
+  let store: Store<State>;
+  let effects: StatusEffects;
+  let actions: Observable<any>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({
+          ...reducers,
+        }),
+      ],
+      providers: [
+        ...PROVIDERS,
+        StatusEffects,
+        provideMockActions(() => actions),
+      ],
+    });
+
+    effects = TestBed.get(StatusEffects);
+    store = TestBed.get(Store);
+  });
+
+  it('should watch for status changes', () => {
+    const fakeStatus = cold('p--a-n---t|', {
+      n: ConnectionStatus.NoNetwork,
+      p: ConnectionStatus.NoProvider,
+      a: ConnectionStatus.NoAccount,
+      t: ConnectionStatus.Total,
+    });
+
+    spyOn(effects, 'getWeb3Status').and.returnValue(fakeStatus);
+
+    actions = hot('-a', {
+      a: {type: ROOT_EFFECTS_INIT},
+    });
+
+    const expected = cold('-p--a-n---t|', {
+      n: new statusActions.SetStatusAction(ConnectionStatus.NoNetwork),
+      p: new statusActions.SetStatusAction(ConnectionStatus.NoProvider),
+      a: new statusActions.SetStatusAction(ConnectionStatus.NoAccount),
+      t: new statusActions.SetStatusAction(ConnectionStatus.Total),
+    });
+
+    expect(effects.watchStatusChanges$).toBeObservable(expected);
   });
 });
