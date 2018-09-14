02b904f0ae03899b200f6bf728d171a95a838033
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2018-09-14 18:10:20 +0200

    Added effects to track transactions sent from the app. #96

--- src/app/shared/store/account/account.actions.ts
+++ src/app/shared/store/account/account.actions.ts
@@ -1,7 +1,13 @@
 import { Action } from '@ngrx/store';
 
-export const SET_ACCOUNT = 'account - set account';
-export const SET_BALANCE = 'account - set balance';
+import { FullTransaction } from '../../web3.service';
+
+export const SET_ACCOUNT =                    'account - set account';
+export const SET_BALANCE =                    'account - set balance';
+export const ADD_NEW_TRANSACTION =            'account - new transaction';
+export const SET_TRANSACTION_METADATA =            'account - set transaction metadata';
+export const REMOVE_NEW_TRANSACTION =         'account - remove transaction';
+export const SET_TRANSACTION_CONFIRMATIONS =  'account - set transaction confirmations';
 
 // Account data
 export class SetAccountAction implements Action {
@@ -13,6 +19,34 @@ export class SetBalanceAction implements Action {
   constructor(public payload: number) { }
 }
 
+// Confirmations
+export class AddNewTransactionAction implements Action {
+  readonly type = ADD_NEW_TRANSACTION;
+  constructor(public payload: string) { }
+}
+export class SetTransactionMetadataAction implements Action {
+  readonly type = SET_TRANSACTION_METADATA;
+  payload: {tx: string, metadata: FullTransaction};
+  constructor(tx: string, metadata: FullTransaction) {
+    this.payload = {tx, metadata};
+  }
+}
+export class RemoveTransactionAction implements Action {
+  readonly type = REMOVE_NEW_TRANSACTION;
+  constructor(public payload: string) { }
+}
+export class SetTransactionConfirmationsAction implements Action {
+  readonly type = SET_TRANSACTION_CONFIRMATIONS;
+  payload: {tx: string, confirmations: number};
+  constructor(tx: string, confirmations: number) {
+    this.payload = {tx, confirmations};
+  }
+}
+
 export type Actions
   = SetAccountAction
-  | SetBalanceAction;
+  | SetBalanceAction
+  | AddNewTransactionAction
+  | SetTransactionMetadataAction
+  | RemoveTransactionAction
+  | SetTransactionConfirmationsAction;
