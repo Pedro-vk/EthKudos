697f6244354f1ebd5682743b315648d659926c17
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2018-09-19 01:17:00 +0200

    Used date from store and removed service dependencies on Active poll views. #98

--- src/app/+app/poll/poll-previous/poll-previous.component.ts
+++ src/app/+app/poll/poll-previous/poll-previous.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/shareReplay';
 
 import * as fromRoot from '../../../shared/store/reducers';
 
@@ -14,7 +15,8 @@ import * as fromRoot from '../../../shared/store/reducers';
 })
 export class PollPreviousComponent {
   readonly kudosToken$ = this.store.select(fromRoot.getCurrentKudosTokenWithFullData)
-    .filter(_ => !!_);
+    .filter(_ => !!_)
+    .shareReplay();
   readonly kudosPoll$ = this.kudosToken$
     .map(({selectedPoll}) => selectedPoll);
 
