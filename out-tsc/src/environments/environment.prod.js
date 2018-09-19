aa1002494e22dbe8a3004c298ae878f7841b1ca3
CircleCI Deploy <deploy@circleci.com>
2018-09-19 15:40:58 +0000

    Built from f976275b3bf9e137: Added pre-production deploying.

--- out-tsc/src/environments/environment.prod.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = {
-    production: true,
-    web3Provider: undefined,
-    defaultGasLimit: undefined,
-};
-//# sourceMappingURL=environment.prod.js.map
