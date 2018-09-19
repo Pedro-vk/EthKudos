e0a11e391d7c5013890b900dd7069037c420b828
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2018-09-19 02:08:47 +0200

    Used date from store and removed service dependencies on Graph poll views. #98

--- src/app/+app/poll/poll-chart/poll-chart.component.ts
+++ src/app/+app/poll/poll-chart/poll-chart.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef, HostListener, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/empty';
-import 'rxjs/add/observable/fromPromise';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/combineLatest';
 import 'rxjs/add/operator/delay';
@@ -10,12 +10,11 @@ import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/first';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/shareReplay';
-import 'rxjs/add/operator/startWith';
 
-import { KudosTokenFactoryService, KudosPollFactoryService } from '../../../shared';
 import { GraphComponent } from '../../../components';
+import { KudosTokenFactoryService, KudosPollFactoryService } from '../../../shared';
+import * as fromRoot from '../../../shared/store/reducers';
 
 @Component({
   selector: 'eth-kudos-poll-chart',
@@ -27,59 +26,37 @@ export class PollChartComponent implements OnInit {
   @ViewChild('graph') graph: GraphComponent;
   @ViewChild('wrapper') wrapper: ElementRef;
 
-  readonly kudosTokenService$ = this.activatedRoute.parent.params
-    .map(({tokenAddress}) => this.kudosTokenFactoryService.getKudosTokenServiceAt(tokenAddress))
+  readonly kudosToken$ = this.store.select(fromRoot.getCurrentKudosTokenWithFullData)
     .filter(_ => !!_)
     .shareReplay();
-  readonly token$ = this.kudosTokenService$.mergeMap(s => s.getTokenInfo());
+  readonly kudosPoll$ = this.kudosToken$
+    .map(({selectedPoll}) => selectedPoll)
+    .filter(_ => !!_);
 
-  readonly pollContract$ = this.activatedRoute.params
-    .filter(({address}) => !!address)
-    .map(({address}) => this.kudosPollFactoryService.getKudosPollServiceAt(address))
-    .mergeMap(kudosPollService => kudosPollService.onInitialized.startWith(undefined).map(() => kudosPollService))
-    .shareReplay();
-  readonly gratitudesEdges$ = this.pollContract$
-    .mergeMap(kudosPollService => kudosPollService.checkUpdates(_ => _.allGratitudes()))
-    .first()
-    .combineLatest(this.kudosTokenService$)
-    .first()
-    .map(([gratitudes, kudosTokenService]) => gratitudes
-      .map(async _ => ({
-        ..._,
-        kudos: await kudosTokenService.fromInt(_.kudos),
-      })),
-    )
-    .mergeMap(_ => Observable.fromPromise(Promise.all(_)))
-    .map(gratitudes => gratitudes.map(({from, to, kudos, message}) => [from, to, kudos, message]))
+  readonly gratitudesEdges$ = this.kudosPoll$
+    .map(({allGratitudes}) => allGratitudes)
+    .map((gratitudes = []) => gratitudes.map(({from, to, kudos, message}) => [from, to, kudos, message]))
     .distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b))
-    .catch(() => Observable.empty())
+    .filter(_ => !!_.length)
     .shareReplay();
-  readonly gratitudesNodes$ = this.pollContract$
-    .mergeMap(kudosPollService => kudosPollService.checkUpdates(_ => _.getMembers()))
-    .first()
-    .combineLatest(this.kudosTokenService$)
-    .map(([members, kudosTokenService]) =>
-      members.map(async member => ({member, name: await kudosTokenService.getContact(member)})),
-    )
-    .mergeMap(_ => Observable.fromPromise(Promise.all(_)))
-    .distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b))
-    .map(nodes =>
-      nodes
-        .map(({member, name}) => ({
+  readonly gratitudesNodes$ = this.kudosPoll$
+    .combineLatest(this.kudosToken$)
+    .map(([{members}, {contacts}]) =>
+      (members || [])
+        .map(member => ({
           id: member,
-          name,
+          name: contacts[member],
           address: member,
         }))
         .sort((a, b) => +a.address - +b.address)
         .map(data => ({data})),
     )
-    .catch(() => Observable.empty())
+    .filter(_ => !!_.length)
     .shareReplay();
 
   constructor(
+    private store: Store<fromRoot.State>,
     public activatedRoute: ActivatedRoute,
-    private kudosTokenFactoryService: KudosTokenFactoryService,
-    private kudosPollFactoryService: KudosPollFactoryService,
     private changeDetectorRef: ChangeDetectorRef,
   ) { }
 
